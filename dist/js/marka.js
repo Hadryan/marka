67629d247165088270e710724d5c9dbdc1fd4eba
Alfiana Sibuea <alfian.sibuea@gmail.com>
2014-10-04 19:25:54 +0700

    Tidy up some code

--- dist/js/marka.js
+++ dist/js/marka.js
@@ -9,179 +9,75 @@
  */ 
 
 ;(function(window) {
-	'use strict';
+    'use strict';
 
     // Blocks needed to create the icon
     var blockList = {
 
-    	/* Circle Icons */
-    	'circle': {
-    		block: 2
-    	},
-    	'circle-o': {
-    		block: 3,
-    		invert: [1]
-    	},
-    	'circle-o-filled': {
-    		block: 3,
-    		invert: [1]
-    	},
+        'circle':          {block: 2 },
+        'circle-o':        {block: 3, invert: [1]},
+        'circle-o-filled': {block: 3, invert: [1]},
 
-    	'circle-minus': {
-    		block: 3,
-    		invert: 'last'
-    	},
-    	'circle-plus': {
-    		block: 3,
-    		invert: 'last-two'
-    	},
-    	'circle-times': {
-    		block: 3,
-    		invert: 'last-two'
-    	},
+        'circle-minus':    {block: 3, invert: 'last'},
+        'circle-plus':     {block: 3, invert: 'last-two'},
+        'circle-times':    {block: 3, invert: 'last-two'},
 
-    	'circle-o-minus': {
-    		block: 4,
-    		invert: [1]
-    	},
-    	'circle-o-plus': {
-    		block: 4,
-    		invert: [1]
-    	},
-    	'circle-o-times': {
-    		block: 4,
-    		invert: [1]
-    	},
+        'circle-o-minus':  {block: 4, invert: [1]},
+        'circle-o-plus':   {block: 4, invert: [1]},
+        'circle-o-times':  {block: 4, invert: [1]},
 
-    	'square': {
-    		block: 2
-    	},
-    	'square-o': {
-    		block: 3,
-    		invert: [1]
-    	},
-    	'square-o-filled': {
-    		block: 3,
-    		invert: [1]
-    	},
+        'square':          {block: 2},
+        'square-o':        {block: 3, invert: [1]},
+        'square-o-filled': {block: 3, invert: [1]},
 
-    	'square-minus': {
-    		block: 3,
-    		invert: 'last'
-    	},
-    	'square-plus': {
-    		block: 3,
-    		invert: 'last-two'
-    	},
-    	'square-times': {
-    		block: 3,
-    		invert: 'last-two'
-    	},
-    	'square-check': {
-    		block: 3,
-    		invert: 'last-two'
-    	},
+        'square-minus':    {block: 3, invert: 'last'},
+        'square-plus':     {block: 3, invert: 'last-two'},
+        'square-times':    {block: 3, invert: 'last-two'},
+        'square-check':    {block: 3, invert: 'last-two'},
 
-    	'square-o-minus': {
-    		block: 4,
-    		invert: [1]
-    	},
-    	'square-o-plus': {
-    		block: 4,
-    		invert: [1]
-    	},
-    	'square-o-times': {
-    		block: 4,
-    		invert: [1]
-    	},
-    	'square-o-check': {
-    		block: 4,
-    		invert: [1]
-    	},
+        'square-o-minus':  {block: 4, invert: [1]},
+        'square-o-plus':   {block: 4, invert: [1]},
+        'square-o-times':  {block: 4, invert: [1]},
+        'square-o-check':  {block: 4, invert: [1]},
 
-    	'triangle': {
-    		block: 3
-    	},
+        'triangle': {block: 3},
+        'pause':    {block: 2},
 
-    	'asterisk': {
-    		block: 3
-    	},
-    	'minus': {
-    		block: 1
-    	},
-    	'plus': {
-    		block: 2
-    	},
-    	'times': {
-    		block: 2
-    	},
+        'asterisk': {block: 3},
+        'minus':    {block: 1},
+        'plus':     {block: 2},
+        'times':    {block: 2},
+        'check':    {block: 2},
 
-    	'check': {
-    		block: 2
-    	},
-    	'sort': {
-    		block: 6
-    	},
-    	'sort-half': {
-    		block: 3
-    	},
+        'sort':         {block: 6},
+        'sort-half':    {block: 3},
 
-    	'signal-three-one': {
-    		block: 3
-    	},
-    	'signal-three-two': {
-    		block: 3
-    	},
-    	'signal-three': {
-    		block: 3
-    	},
-    	'signal-five-one': {
-    		block: 5
-    	},
-    	'signal-five-two': {
-    		block: 5
-    	},
-    	'signal-five-three': {
-    		block: 5
-    	},
-    	'signal-five-four': {
-    		block: 5
-    	},
-    	'signal-five': {
-    		block: 5
-    	},
+        'signal-three-one':     {block: 3},
+        'signal-three-two':     {block: 3},
+        'signal-three':         {block: 3},
+        'signal-five-one':      {block: 5},
+        'signal-five-two':      {block: 5},
+        'signal-five-three':    {block: 5},
+        'signal-five-four':     {block: 5},
+        'signal-five':          {block: 5},
 
-    	'pause': {
-    		block: 2
-    	},
-
-    	'angle': {
-    		block: 2
-    	},
-    	'angle-double': {
-    		block: 4
-    	},
-    	'arrow': {
-    		block: 3
-    	},
-    	'bars': {
-    		block: 3
-    	},
-    	'chevron': {
-    		block: 2
-    	},
+        'angle':        {block: 2},
+        'angle-double': {block: 4},
+        'arrow':        {block: 3},
+        'bars':         {block: 3},
+        'chevron':      {block: 2},
     };
 
     function applyFunc(el, callback) {
-    	return Array.prototype.forEach.call(el, callback);
+        return Array.prototype.forEach.call(el, callback);
     }
 
     function isElement(o){
-		return (
-			typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
-			o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName==="string"
-		);
-	}
+        return (
+            typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
+            o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName==="string"
+        );
+    }
 
     function changeColor(el, color) {
 
@@ -243,134 +139,134 @@
         el.setAttribute('data-bg', backgroundColor);
     }
 
-	function MarkaIcon(el) {
-		
-		this.elements = [];
+    function MarkaIcon(el) {
+        
+        this.elements = [];
 
-		if (typeof el === 'string') {
-			this.elements = document.querySelectorAll(el);
-		}
+        if (typeof el === 'string') {
+            this.elements = document.querySelectorAll(el);
+        }
 
-		if (isElement(el)) {
-			this.elements.push(el);
-		}
+        if (isElement(el)) {
+            this.elements.push(el);
+        }
 
-		if (el instanceof Array) {
-			for (var a = 0; a < el.length; a++) {
-				if (isElement(el[a])) {
-					this.elements.push(el[a]);
-				}
-			}
-		}
+        if (el instanceof Array) {
+            for (var a = 0; a < el.length; a++) {
+                if (isElement(el[a])) {
+                    this.elements.push(el[a]);
+                }
+            }
+        }
 
-		if (!this.elements.length) {
-			throw Error('No element is selected.');
-		}
+        if (!this.elements.length) {
+            throw Error('No element is selected.');
+        }
 
-		applyFunc(this.elements, function(i) {
+        applyFunc(this.elements, function(i) {
 
-			// Set background color
+            // Set background color
             setBackground(i);
 
-			if (i.className.indexOf('marka') === -1) {
-				i.className += ' marka ';
-			}
-		});
+            if (i.className.indexOf('marka') === -1) {
+                i.className += ' marka ';
+            }
+        });
 
-		return this;
+        return this;
 
-	}
+    }
 
-	MarkaIcon.prototype.set = function(icon) {
+    MarkaIcon.prototype.set = function(icon) {
 
-		var el = this;
+        var el = this;
 
-		applyFunc(this.elements, function(i) {
+        applyFunc(this.elements, function(i) {
 
-			// Set icon name
-			i.setAttribute('data-icon', icon);
+            // Set icon name
+            i.setAttribute('data-icon', icon);
 
-			// Get data-color
-			var color = i.getAttribute('data-color');
+            // Get data-color
+            var color = i.getAttribute('data-color');
 
-			if (!color) {
-				color = 'rgb(0, 0, 0)';
-				i.setAttribute('data-color', color);
-			}
+            if (!color) {
+                color = 'rgb(0, 0, 0)';
+                i.setAttribute('data-color', color);
+            }
 
-			var blockCount = i.children.length;
+            var blockCount = i.children.length;
 
-			// Append blocks
-			if (blockList[icon].block > blockCount) {
-				for (var a = 0; a < (blockList[icon].block - blockCount); a++) {
+            // Append blocks
+            if (blockList[icon].block > blockCount) {
+                for (var a = 0; a < (blockList[icon].block - blockCount); a++) {
 
-					var span = document.createElement('i');
+                    var span = document.createElement('i');
 
-					i.appendChild(span);
-				}
-			}
+                    i.appendChild(span);
+                }
+            }
 
             // Change color
             setBackground(i);
             changeColor(i, color);
 
-			// Prevent blink transition
-			setTimeout(function() {
+            // Prevent blink transition
+            setTimeout(function() {
 
-				// Change class			
-				i.className = i.className.replace('  ', ' ').replace(/marka-icon-[\w-]+/, '');
-				i.className += 'marka-icon-'+icon+' ';
+                // Change class         
+                i.className = i.className.replace('  ', ' ').replace(/marka-icon-[\w-]+/, '');
+                i.className += 'marka-icon-'+icon+' ';
 
-				if ('sizeValue' in el) {
-					i.setAttribute('style', 'width:'+el.sizeValue+'px;height:'+el.sizeValue+'px;');
-				}
+                if ('sizeValue' in el) {
+                    i.setAttribute('style', 'width:'+el.sizeValue+'px;height:'+el.sizeValue+'px;');
+                }
 
-				// Show icon if it's not shown
-				if (i.className.indexOf('marka-set') === -1) {
-					setTimeout(function() {
-						i.className += 'marka-set ';
-					}, 200);
-				}
+                // Show icon if it's not shown
+                if (i.className.indexOf('marka-set') === -1) {
+                    setTimeout(function() {
+                        i.className += 'marka-set ';
+                    }, 200);
+                }
 
-			}, 10);
+            }, 10);
 
-		});
+        });
 
-		return this;
-	};
+        return this;
+    };
 
-	MarkaIcon.prototype.color = function(color) {
+    MarkaIcon.prototype.color = function(color) {
 
-		applyFunc(this.elements, function(i) {
+        applyFunc(this.elements, function(i) {
             setBackground(i);
             changeColor(i, color);
-		});	
+        }); 
 
-		return this;
-	};
+        return this;
+    };
 
-	MarkaIcon.prototype.size = function(size) {
+    MarkaIcon.prototype.size = function(size) {
 
-		this.sizeValue = size;
+        this.sizeValue = size;
 
-		applyFunc(this.elements, function(i) {
-			i.setAttribute('style', 'width:'+size+'px;height:'+size+'px;');
-		});	
+        applyFunc(this.elements, function(i) {
+            i.setAttribute('style', 'width:'+size+'px;height:'+size+'px;');
+        }); 
 
-		return this;
-	};
+        return this;
+    };
 
-	MarkaIcon.prototype.rotate = function(direction) {
+    MarkaIcon.prototype.rotate = function(direction) {
 
-		applyFunc(this.elements, function(i) {
-			i.className = i.className.replace('  ', ' ').replace(/marka-rotate-[\w]+/, '');
-			i.className += 'marka-rotate-'+direction+' ';
-		});	
+        applyFunc(this.elements, function(i) {
+            i.className = i.className.replace('  ', ' ').replace(/marka-rotate-[\w]+/, '');
+            i.className += 'marka-rotate-'+direction+' ';
+        }); 
 
-		return this;
-	};
+        return this;
+    };
 
-	window.Marka = function(el) {
+    window.Marka = function(el) {
         var marka = new MarkaIcon(el);
         return marka;
     };
@@ -386,7 +282,7 @@
                     el.push(obj[a]);
                 }
             }
-            console.log(options);
+            
             var marka = window.Marka(el);
 
             if ('set' in options) {
